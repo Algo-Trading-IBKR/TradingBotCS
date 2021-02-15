733e005eed0b5f36b77b55c1019f48bb78b648fa
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2021-02-15 11:07:42 +0100

    add account info endpoint

---
+++ TradingBotCS/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingBotCS.Database;
+using TradingBotCS.DataModels;
+
+namespace TradingBotCS.Controllers
+{
+    [ApiController]
+    [Route("account")]
+    public class AccountController : ControllerBase
+    {
+        public AccountController() { }
+
+        [HttpGet] 
+        public ActionResult GetTest()
+        {
+            return new OkObjectResult("test");
+        }
+
+        [HttpGet]
+        [Route("{key}/{allItems}")]
+        public async Task<ActionResult> GetAccountInfo(string key, bool allItems = false)
+        {
+            List<AccountInfo> Result = await AccountRepository.ReadAccountUpdate(key, allItems);
+            return new OkObjectResult(Result);
+        }
+    }
+}
