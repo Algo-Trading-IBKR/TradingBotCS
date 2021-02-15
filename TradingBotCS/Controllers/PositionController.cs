78bd6d8a10f682b62285002716846b9c0d73e710
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2021-02-15 11:07:50 +0100

    add position endpoint

---
+++ TradingBotCS/Controllers/PositionController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingBotCS.Database;
+using TradingBotCS.DataModels;
+
+namespace TradingBotCS.Controllers
+{
+    [ApiController]
+    [Route("position")]
+    public class PositionController : ControllerBase
+    {
+        public PositionController() { }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<IActionResult> GetAllPositions()
+        {
+            try
+            {
+                List<Position> Results = await PositionsRepository.ReadPositions(allItems: true);
+                return new OkObjectResult(Results);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+
+        [HttpGet]
+        [Route("{symbol}")]
+        public async Task<IActionResult> GetPosition(string symbol)
+        {
+            try
+            {
+                Console.WriteLine(symbol);
+                List<Position> Results = await PositionsRepository.ReadPositions(symbol);
+                return new OkObjectResult(Results);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+
+    }
+}
